f478a82dc398c329e97e6fac446721a4ab5f2906
UsusCimex <daniil8914mail@mail.ru>
2025-09-21 14:19:53 +0700

    add lab1.3

---
+++ Clojure/1.3/program.clj
@@ -0,0 +1,23 @@
+(defn my-map [f coll]
+  (reduce
+   (fn [acc x] (concat acc (list (f x))))  ; добавляем элемент в конец
+   (list)                                  ; начальный аккумулятор
+   coll))
+
+(println (my-map inc (list 1 2 3 4)))
+; => (2 3 4 5)
+
+(println (my-map (fn [x] (* x x)) (list 1 2 3 4)))
+; => (1 4 9 16)
+
+(defn my-filter [pred coll]
+  (reduce
+   (fn [acc x]
+     (if (pred x)
+       (concat acc (list x))   ; добавляем только если pred(x) true
+       acc))
+   (list)
+   coll))
+
+(println (my-filter (fn [n] (= 0 (mod n 3))) (range 1 21)))
+; => (3 6 9 12 15 18)
